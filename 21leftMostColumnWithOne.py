de15d738b556a16c1d13847bd9d1c21a9251aa42
XtarX <xtarx0@gmail.com>
2020-04-23 15:19:17 +0200

    21

--- 21leftMostColumnWithOne.py
+++ 21leftMostColumnWithOne.py
@@ -2,8 +2,9 @@ from typing import List
 
 
 class BinaryMatrix(object):
-    # m = [[0, 0, 0, 1], [0, 0, 1, 1], [0, 1, 1, 1]]
-    # m=
+    m = [[0, 0, 0, 1],
+         [0, 0, 1, 1],
+         [0, 1, 1, 1]]
     acc = 0
 
     def get(self, x: int, y: int) -> int:
@@ -16,33 +17,23 @@ class BinaryMatrix(object):
 
 class Solution:
     def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        rows, cols = binaryMatrix.dimensions()
 
-        m, n = binaryMatrix.dimensions()
-        print(m, n)
-        rowsSums = dict()
-        lastColZerosCount = 0
-        # check if the last column has any 1s, if not then return -1
-        for x in range(0, m):
-            if binaryMatrix.get(x, n - 1) > 0:
-                rowsSums[x] = rowsSums.get(x, 0) + 1
-                # break
+        currentRow = 0
+        currentColumn = cols - 1
+
+        # if 1 move left,otherwise move down
+        while currentRow < rows and currentColumn > -1:
+            if binaryMatrix.get(currentRow, currentColumn) == 1:
+                currentColumn -= 1
             else:
-                lastColZerosCount += 1
+                currentRow += 1
 
-        if lastColZerosCount == m:
-            return -1
-        # print(rowsSums)
-        for x in range(0, m):
-            for y in range(n - 2, -1, -1):
-                if binaryMatrix.get(x, y) > 0:
-                    rowsSums[x] = rowsSums.get(x, 0) + 1
-                else:
-                    break
+        print("called", binaryMatrix.acc)
+        if currentColumn != cols - 1:
+            return currentColumn + 1
 
-        # print(rowsSums)
-        print("BinaryMatrix.get() called:", binaryMatrix.acc)
-
-        return n - max(rowsSums.values())
+        return -1
 
 
 s = Solution();
